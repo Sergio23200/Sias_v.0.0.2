07a80e94523c291cee7fb3537590313e6f5755a1
deisy <deisycarolinasoto15@gmail.com>
2024-09-06 22:23:41 -0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -64,31 +64,35 @@ def get_current_Admin(token: str = Depends(OAuth2_schema)):
 
 class JWTBearer(HTTPBearer):
     async def __call__(self, request: Request):
+        # validad token y verificar su validez
         auth = await super().__call__(request)
         data = validate_token(auth.credentials)
 
         try:
             validate = Affiliate.get(Affiliate.email == data["email"])
         except Affiliate.DoesNotExist:
+            # valida si el afiliante existe en base de datos
             try:
                 validate = Admin.get(Admin.email == data["email"])
             except Admin.DoesNotExist:
                 raise HTTPException(
                     status_code=403, detail="Credenciales inválidas"
+                    # valida si las credenciales son validas, si no aparece "credenciales inválidas"
                 )
 
         return validate
+    # validador
 
 
 def model_to_dict(model_instance):
     """
-    Convert a Peewee model instance to a dictionary, handling non-serializable types.
+    Convertir una instancia del modelo Peewee en un diccionario, manejando tipos no serializables
     """
     data = {}
     for field in model_instance._meta.sorted_field_names:
         value = getattr(model_instance, field)
         if isinstance(value, (datetime, date)):
-            # Convert date or datetime to ISO format string
+            # Convertir fecha y hora a cadena de formato ISO
             data[field] = value.isoformat()
         else:
             data[field] = value
@@ -128,10 +132,13 @@ async def user_delete(current_user: Affiliate = Depends(get_current_Affiliate)):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="El proceso no se puede completar"
+            # esta funcion es creada para decir que los requisitos no estan completas y aparecera "el proceso no se puede completar"
         )
     return {"detail": "El usuario ha sido eliminado"}
+#esta funcion le muestra a la persona que "el usuario que ha sido eliminado"
 
 
+#crear un usuario
 @ app.post("/user/create", tags=["CRUD Affiliate"])
 async def create_user(
     fullname: str = Form(...),
@@ -170,12 +177,15 @@ async def create_user(
         if not new_user:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST, detail="Error al crear el usuario")
+        # esta funcion muesta si hay algun errror al crear un usuario
 
         return {"detail": "El usuario se ha creado con éxito"}
+    # esta funcion te indica que el usuario fue creado correctamente
 
     except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=f"Error interno: {str(e)}")
+    # esta funcion es cuendo el servidor presenta un error interno
 
 
 @app.put("/user/update", tags=["CRUD Affiliate"], dependencies=[Depends(JWTBearer())])
@@ -201,12 +211,14 @@ async def affiliate_update(
     if not update_result:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="El usuario no se pudo actualizar"
+            # esta funcion te muestra cuando el usuario no se pudo actualizar
         )
 
     return {"detail": "Affiliate updated successfully"}
 # CRUD Admin
 
 
+# craer admin
 @ app.post("/admin/create",  tags=["CRUD Admin"])
 async def create_admin(
         fullname: str = Form(...),
@@ -230,23 +242,28 @@ async def create_admin(
                       job_title=job_title)
         new_admin = CD.create_admin(admin)
         return {"detail": "El usuario se ha creado con éxito"}
+    # esta funcion te indica si el usuario es creado correctamente
     except:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="Error al crear el usuario")
+    # estafuncion te indica si el usuario tuvo un error al crearse 
 
-
+# eliminar admin
 @app.delete("/admin/delete", tags=["CRUD Admin"], dependencies=[Depends(JWTBearer())])
 async def delete_admin(current_user: Affiliate = Depends(get_current_Affiliate)):
     try:
         email = current_user.email
         rows_delete = CD.delete_Admin(email)
         return {"detail": "El usuario ha sido eliminado"}
+    # esta funcion te indica que el usuario ha sido eliminado
     except:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="El proceso no se puede completar")
+    # esta funcion es para cuando el proceso no se puede completar
 
 
+#actualizar admin
 @app.put("/admin/update", tags=["CRUD Admin"], dependencies=[Depends(HTTPBearer())])
 async def update_admin(
         first_number: str = Form(...),
@@ -267,6 +284,7 @@ async def update_admin(
     if not update_result:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="El usuario no se pudo actualizar"
+            # este funcion te indica de los cambios no se guardaron y aparece así "el usuario no se pudo actualizar"
         )
 
     return {"detail": "Affiliate updated successfully"}
@@ -299,10 +317,12 @@ async def create_Specialist(fullname: str = Form(...),
     if not new_specialist:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail="El especialista no pudo ser creado")
+    # esta funcion te indica cuando el especialista no se pudo crear
 
     return {"message": "Especialista creado exitosamente"}
 
 
+# actualizar specialist
 @app.put("/Specialist/update", tags=["CRUD Specialist"], dependencies=[Depends(HTTPBearer())])
 async def specialist_update_(number_document: int = Form(...),
                              email: str = Form(...),
@@ -318,20 +338,24 @@ async def specialist_update_(number_document: int = Form(...),
     if not update_result:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="El usuario no se pudo actualizar"
+            # esta funcion te indica cuando el usuario no se pudo actualizar en su base de datos
         )
 
     return {"detail": "Affiliate updated successfully"}
 
-
+# eliminar specialist
 @app.delete("/Speialist/delete", tags=["CRUD Specialist"], dependencies=[Depends(HTTPBearer())])
 async def specialist_delete(number_document: int = Form(...)):
     try:
         rows = CD.delete_specialst(number_document)
         return {"detail": "El usuario ha sido eliminado"}
+    # indica que se ha eliminado correctamente el usuario
     except:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="El proceso no se puede completar")
+    # esta funcion indica que el proceso no se pudo completar
+    
 # CRUD hospital
 
 
@@ -358,10 +382,13 @@ async def created_Hospital(fullname: str = Form(...),
     if not new_hospital:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail="El hospital no pudo ser creado")
+    # esta funcion indica que el hospital en base de datos no pudo ser creado
 
     return {"message": "hospital creado exitosamente"}
+# esta funcion indica que el hospital fue creado exitosamente
 
 
+#actualizar hospital
 @app.put("/hospital/update", tags=["CRUD hospital"], dependencies=[Depends(HTTPBearer())])
 async def update_Hospotal(fullname: str = Form(...),
                           email: str = Form(...),
@@ -376,23 +403,26 @@ async def update_Hospotal(fullname: str = Form(...),
     if not update_result:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="El  hospital no se pudo actualizar"
+            # esta funcion indica que el hospital no pudo ser actualizado
         )
 
     return {"detail": "el hospital updated successfully"}
 
-
+# eliminar hospital
 @app.delete("/hospital/update", tags=["CRUD hospital"], dependencies=[Depends(HTTPBearer())])
 async def delete_Hospital(fullname: str = Form(...)):
     try:
         rows = CD.delete_hospital(fullname)
         return {"detail": "El hospital ha sido eliminado"}
+    # esta funcion indica que el hospital ha sido eliminado de base de datos
     except:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="El hospital no se puede eliminar")
+    # esta funcion te indica que el hospital no se ha podido eliminar
 # CRUD IPS
 
-
+#crear IPS
 @app.post("/IPS/create", tags=["CRUD ips"], dependencies=[Depends(HTTPBearer())])
 async def ips_create(fullname: str = Form(...),
                      city: str = Form(...),
@@ -412,10 +442,13 @@ async def ips_create(fullname: str = Form(...),
     if not new_ips:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail="ips no pudo ser creado")
+    # esta funcion indica que el IPS no pudo ser creado
 
     return {"message": "ips creado exitosamente"}
+# esta funcion te informa cuando el IPS es creado correctamente
 
 
+#actualizar IPS
 @app.put("/IPS/update", tags=["CRUD ips"], dependencies=[Depends(HTTPBearer())])
 async def Ips_update(fullname: str = Form(...),
                      email: str = Form(...),
@@ -430,19 +463,24 @@ async def Ips_update(fullname: str = Form(...),
             status_code=status.HTTP_400_BAD_REQUEST, detail="El  hospital no se pudo actualizar"
         )
     return {"detail": "Ips updated successfully"}
+# esta funcion te informa cuando la IPS del hospita no pudo ser actualizada
 
 
+# eliminar IPS
 @app.delete("/IPS/update", tags=["CRUD ips"], dependencies=[Depends(HTTPBearer())])
 async def delete_IPS(fullname: str = Form(...)):
     try:
         rows = CD.delete_ips(fullname)
         return {"detail": "El hospital ha sido eliminado"}
+    # esta funcion informa cuando la IPS del hospital ha sido eliminada
     except:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="El hospital no se puede eliminar")
-# CRUD medical_appointments
+    # esta funcion te indica cuando el IPS del hospital no se puede eliminar
 
+
+# CRUD medical_appointments
 
 @app.post("/appointments/create", tags=["CRUD appointments"], dependencies=[Depends(HTTPBearer)])
 async def create_appointments(appointment_type: str = Form(...),
